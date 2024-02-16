fc6dd7df35056a42376e845fb47421dca952b3e5
Aditya Padgal <adityapadgal@Adityas-MacBook-Air-2.local>
2024-02-15 19:16:19 -0500

    updated test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { add } = require("../arithmetica")
 const port = 3001;
 app.use(cors())
 app.get('/', (req, res) => {
-    res.send('Arithmetic Service - Hello World!');
+    res.send('Arithmetic Service - last updated ');
 });
+// app.get('/add/:n/:m', (req, res) => {
+
+//     res.json(Number(req.params.n) + Number(req.params.m));
+// });
+
 app.get('/add/:n/:m', (req, res) => {
-    res.json(Number(req.params.n) + Number(req.params.m));
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let sum  = add(n,m);
+        res.json(sum);
 });
 
 app.get('/sub/:n/:m', (req, res) => {
---
+++ src/test/arithmetica.test.js
@@ -0,0 +1,4 @@
+const { add } = require("../arithmetica")
+test('2+3 is equal to 5', () => {
+    expect(add(2,3)).toBe(5);
+});
