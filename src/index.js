6c0ec0e469e16ca84fa208f10fc04cfc71a7c132
adityapadgal <53577378+adityapadgal@users.noreply.github.com>
2024-03-09 22:23:59 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { add } = require("../arithmetica")
+const { add } = require("./arithmetica")
 const port = 3001;
 app.use(cors())
 app.get('/', (req, res) => {
